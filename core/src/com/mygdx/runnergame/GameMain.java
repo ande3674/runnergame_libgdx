7edbb2da93ba18c28972154fde23c4b163616f70
ande3674 <ande3674@gmail.com>
2018-12-08 23:58:07 -0600

    Add music to the game

--- core/src/com/mygdx/runnergame/GameMain.java
+++ core/src/com/mygdx/runnergame/GameMain.java
@@ -3,6 +3,7 @@ package com.mygdx.runnergame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,10 +12,16 @@ import scenes.Gameplay;
 
 public class GameMain extends Game {
 	private SpriteBatch batch;
+	private Music music;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		// do the music
+		music = Gdx.audio.newMusic(Gdx.files.internal("Sounds/music.mp3"));
+		music.setLooping(true);
+		music.play();
+		// open new game screen
 		setScreen(new Gameplay(this));
 	}
 
