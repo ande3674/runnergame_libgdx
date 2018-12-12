138391d7a3051a66d86edd960abbb0a4bf3516e5
ande3674 <ande3674@gmail.com>
2018-12-11 20:58:37 -0600

    add singleton game manager

---
+++ core/src/helpers/GameManager.java
@@ -0,0 +1,26 @@
+package helpers;
+
+public class GameManager {
+
+    /*
+
+    THIS IS A SINGLETON PATTERN
+    The game mananger will take care of parts of the game that need to be managed by a Singleton
+
+     */
+    private static final GameManager ourInstance = new GameManager();
+
+    // create some variables to control the game...
+    // Game started from main means we set up initial values
+    // game started from pause means we preserve lives, coins and score...
+    public boolean gameStartedFromMain, isPaused;
+    public int lifeScore, coinScore, score;
+
+    private GameManager() {
+    }
+
+    public static GameManager getInstance() {
+        return ourInstance;
+    }
+
+}
